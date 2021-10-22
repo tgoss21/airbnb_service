204adf39be5ce2c16c1de487d0713fd89c7c965d
Tracy Goss <64561367+tgoss21@users.noreply.github.com>
2021-10-22 06:04:40 -0700

    Update airbnb_service_gui.py

--- airbnb_service_gui.py
+++ airbnb_service_gui.py
@@ -1,37 +1,38 @@
 import tkinter as tk
 from tkinter import *
+from tkinter import messagebox
+import tkinter
+from airbnb_service import calculate_total
 
 window = Tk()
 window.geometry("500x500")
 window.title("Airbnb Service")
+frame = Frame(window)
+
+label1 = Label(window, text="Airbnb Services", pady=25, font=("arial", 16, "bold",)).pack()
+
+
+def helloCallBack():
+       msg = messagebox.showinfo( "Text")
+       
+def calculate():
+    calulationFee = calculate_total()
+    messagebox.showinfo("Calculation", calulationFee)
+
+B1 = Button(window, text = "Calculate Cleaning Fee", width=20, height=2, pady=1, command = calculate)
+# B1.place(x = 20,y = -20)
+B1.pack()
+
+B2 = Button(window, text = "Update Calendar", width=20, height=2, pady=1, command = helloCallBack)
+# B2.place(x = 50,y = 50)
+B2.pack()
+
+B3 = Button(window, text = "Notify Cleaners", width=20, height=2, pady=1, command = helloCallBack)
+# B1.place(x = 20,y = -20)
+B3.pack()
+
+B4 = Button(window, text = "Edit Cleaning", width=20, height=2, pady=1, command = helloCallBack)
+# B2.place(x = 50,y = 50)
+B4.pack()
+
 window.mainloop()
-
-class karl( Frame ):
-    def __init__( self ):
-        tk.Frame.__init__(self)
-        self.pack()
-        self.master.title("Karlos")
-        self.button1 = Button( self, text = "CLICK HERE", width = 45,
-                               command = self.new_window )
-        self.button1.grid( row = 0, column = 1, columnspan = 2, sticky = W+E+N+S )
-        
-    def new_window(self):
-        self.newWindow = karl2()
-        
-class karl2(Frame):     
-    def __init__(self):
-        new =tk.Frame.__init__(self)
-        new = Toplevel(self)
-        new.title("karlos More Window")
-        new.button = tk.Button(  text = "PRESS TO CLOSE", width = 25,
-                                 command = self.close_window )
-        new.button.pack()
-    
-    def close_window(self):
-        self.destroy()
-
-def main(): 
-    pass
-    
-if __name__ == '__main__':
-    main()
